a34320b3eb0f4e5b84cb4552ff55bcc3b380a7f5
Patrik1996 <pahg1600@student.miun.se>
2019-09-14 11:49:14 +0200

    A simple list in the first row of the window, its interface will be changed later and more functions will be made after the join function is done

--- Skeleton/src/WindowProgram.java
+++ Skeleton/src/WindowProgram.java
@@ -1,19 +1,23 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
 
 import se.miun.distsys.GroupCommuncation;
 import se.miun.distsys.listeners.ChatMessageListener;
 import se.miun.distsys.messages.ChatMessage;
 
+import javax.print.DocFlavor.STRING;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 //Skeleton code for Distributed systems 9hp, DT050A
 
@@ -22,7 +26,10 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 	JFrame frame;
 	JTextPane txtpnChat = new JTextPane();
 	JTextPane txtpnMessage = new JTextPane();
-	
+
+	//an example of client names you will have in the list
+	String[] clients = {"user1", "user2", "user3", "4","5", "6"};
+
 	GroupCommuncation gc = null;	
 
 	public static void main(String[] args) {
@@ -44,13 +51,22 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 		gc = new GroupCommuncation();		
 		gc.setChatMessageListener(this);
 		System.out.println("Group Communcation Started");
+			// test to add users
+			
 	}
 
 	private void initializeFrame() {
 		frame = new JFrame();
+
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		frame.getContentPane().setLayout(new GridLayout(4, 1, 0, 0));
+		
+		/*Show a list of clients in the first row, if you have many users so '
+		will you be able to scroll*/
+		JList<String> joined_clients = new JList<String>(clients);
+		JScrollPane j_clients = new JScrollPane(joined_clients);
+		frame.getContentPane().add(j_clients);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		frame.getContentPane().add(scrollPane);
@@ -66,6 +82,8 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 		btnSendChatMessage.setActionCommand("send");
 		
 		frame.getContentPane().add(btnSendChatMessage);
+
+
 		
 		frame.addWindowListener(new java.awt.event.WindowAdapter() {
 	        public void windowClosing(WindowEvent winEvt) {
